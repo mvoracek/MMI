81f6bb55167cad219f3650a70cb84a3b25756e82
mjvoracek <mjvoracek@gmail.com>
2014-02-03 15:44:26 -0600

    Initial Commit

---
+++ MMI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MMI
+//
+//  Created by Matthew Voracek on 2/3/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MMI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MMI
+//
+//  Created by Matthew Voracek on 2/3/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
